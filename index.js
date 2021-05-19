13a9fb463de527607f96e8f3a1b00640b13a0f82
jasonemarkson <jasonmarkson76@gmail.com>
2021-05-19 19:54:36 -0400

    commit tree d510d48ad317a1e9bfec0f2ae9873c6faeec0124

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  const time = parseInt(string.split(":").join(" "))
+  
+  if (time > 12 && time < 19) {
+    return "Good Afternoon"
+  } else if (time > 0 && time < 12) {
+    return "Good Morning"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message
+}
